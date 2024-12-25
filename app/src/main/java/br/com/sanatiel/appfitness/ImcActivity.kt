751e976bc6aafd626b56327163bfc65917b6ee7f
Sanatiel <sanatielsg@gmail.com>
2024-12-25 19:38:55 -0300

    validating form imc

--- app/src/main/java/br/com/sanatiel/appfitness/ImcActivity.kt
+++ app/src/main/java/br/com/sanatiel/appfitness/ImcActivity.kt
@@ -1,14 +1,39 @@
 package br.com.sanatiel.appfitness
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class ImcActivity : AppCompatActivity() {
+    private lateinit var edtWeight: EditText
+    private lateinit var edtHeight: EditText
+    private lateinit var btnCalc: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
+
+        edtWeight = findViewById(R.id.edt_imc_weight)
+        edtHeight = findViewById(R.id.edt_imc_height)
+        btnCalc = findViewById(R.id.btn_imc_calc)
+
+        btnCalc.setOnClickListener {
+            if(!validateForm()){
+                Toast.makeText(this, R.string.invalid_fields_form, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+
+        }
+    }
+
+    private fun validateForm(): Boolean{
+       return (edtWeight.text.toString().isNotEmpty()
+                && edtHeight.text.toString().isNotEmpty()
+                && !edtWeight.text.toString().startsWith("0")
+                && !edtHeight.text.toString().startsWith("0")
+              )
     }
 }
