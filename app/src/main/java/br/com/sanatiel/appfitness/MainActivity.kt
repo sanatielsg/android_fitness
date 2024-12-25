f01cbc6e2a97c52356404cd0619836ae0577006a
Sanatiel <sanatielsg@gmail.com>
2024-12-25 13:00:48 -0300

    navigate to ImcActivity

--- app/src/main/java/br/com/sanatiel/appfitness/MainActivity.kt
+++ app/src/main/java/br/com/sanatiel/appfitness/MainActivity.kt
@@ -1,14 +1,26 @@
 package br.com.sanatiel.appfitness
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnImc : LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnImc = findViewById(R.id.btn_imc)
+
+        btnImc.setOnClickListener{
+            openActivity()
+        }
+    }
+
+    private fun openActivity(){
+        startActivity(Intent(this, ImcActivity::class.java))
     }
 }
